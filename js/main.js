468a765ed0cc876aa20383a3419767f9e9d88a23
AceCat <myers.benn@gmail.com>
2017-05-21 13:42:02 -0500

    score keeping added

--- js/main.js
+++ js/main.js
@@ -25,13 +25,24 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var scoreDisplay = document.getElementById("score-display");
+var score = 0;
+
+var updateScore = function () {
+	scoreDisplay.textContent = ("SCORE: " + score);
+}
 
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		score++;
+		alert("You found a match! We're automatically resetting the board for you. Keep that score climbing!");
+		updateScore();
+		reset();
 	} else {
-		alert("No match, sorry!");
+		alert("No match, sorry! Press the reset button if you want to try again");
+		score = 0;
+		updateScore();
 		}
 	}
 }
@@ -43,7 +54,6 @@ var flipCard = function() {
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	this.setAttribute("src", cards[cardId].cardImage);
-
 	checkForMatch();
 };
 
@@ -53,9 +63,20 @@ var createBoard = function() {
 		cardElement.setAttribute("src", "images/back.png");
 		cardElement.setAttribute("data-id", i);
 		cardElement.addEventListener("click", flipCard);
+		var resetButton = document.getElementById("reset");
+		resetButton.addEventListener("click", reset);
 		document.getElementById("game-board").appendChild(cardElement);
 	}
 }
 
+var reset = function () {
+	cardsInPlay.length = 0;
+	document.getElementById("game-board").innerHTML = "";
+	createBoard();
+};
+
+
+
 createBoard();
+updateScore();
 
